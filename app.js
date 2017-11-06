35b1d9a7aade419139d87318d972c80f7929c5ba
Soprano23 <maurizio.molinari23@gmail.com>
2017-11-06 15:05:48 +0100

    [test]

--- app.js
+++ app.js
@@ -110,6 +110,7 @@ function handlerOrchestratorCall(messageArgs,session,message){
         
         }else{
             //session.send("CHIAMATA EFFETTUATA CORRETTAMENTE",message);
+            session.send("Record trovati: "+body.message,message);
             var bodyParsed = JSON.parse(body);
             
             session.send("Record trovati: "+bodyParsed.message,message);
